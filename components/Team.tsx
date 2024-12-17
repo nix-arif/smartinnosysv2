7c7933a6503cb2fcd04fa1e049853ca9a8f4e52f
nix@rif <arif@nixs-MacBook-Pro.local>
2024-12-17 09:58:25 +0800

    ceo desc

--- components/Team.tsx
+++ components/Team.tsx
@@ -8,7 +8,7 @@ const ourTeam = [
   {
     image: "/1.png",
     alt: "Nik Fikry",
-    text: "Our CEO, Nik Fikry, is a visionary leader with a strong track record of driving innovation and growth. With extensive experience in medical industry, he are committed to steering the company towards continued success, fostering a culture of excellence, and delivering value to our clients, partners, and stakeholders. Under [his/her/their] leadership, we are focused on achieving our strategic goals while maintaining our core values of integrity, collaboration, and customer-centricity.",
+    text: "Our CEO, Nik Fikry, is a visionary leader with a strong track record of driving innovation and growth. With extensive experience in medical industry, he are committed to steering the company towards continued success, fostering a culture of excellence, and delivering value to our clients, partners, and stakeholders. Under his leadership, we are focused on achieving our strategic goals while maintaining our core values of integrity, collaboration, and customer-centricity.",
     name: "Nik Fikry",
     position: "CEO",
   },
